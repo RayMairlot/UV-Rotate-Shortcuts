1979dd9a0837eb086c317bcad34fd7760f978434
Ray Mairlot <madog_theone@hotmail.co.uk>
2014-10-21 12:19:27 +0100

    Initial commit of code

---
+++ UV Rotate.py
@@ -0,0 +1,88 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Rotate UV Selection",
+    "description": "Rotate UV selection left or right by 90 degrees",
+    "author": "Ray Mairlot",
+    "version": (1, 0),
+    "blender": (2, 72, 0),
+    "location": "UV/Image Editor > Shift+R or Ctrl+Shift+R",
+    "category": "UV"}
+    
+import bpy    
+        
+
+class RotateUVLeftOperator(bpy.types.Operator):
+    """ Rotate UV selection to the left """
+    bl_idname = "uv.rotate_selection_left"
+    bl_label = "Rotate UV Selection Left"
+
+
+    def execute(self, context):
+        main(context,"Left")
+        return {'FINISHED'}
+    
+    
+class RotateUVRightOperator(bpy.types.Operator):
+    """ Rotate UV selection to the right """
+    bl_idname = "uv.rotate_selection_right"
+    bl_label = "Rotate UV Selection Right"
+
+
+    def execute(self, context):
+        main(context, "Right")
+        return {'FINISHED'}    
+
+
+def main(context, direction=""):        #Same function used for both operators, pass in direction
+    
+    if direction=="Left":
+        bpy.ops.transform.rotate(value=-1.5708)
+    else:
+        bpy.ops.transform.rotate(value=1.5708)        
+
+
+def register():
+    bpy.utils.register_class(RotateUVLeftOperator)
+    bpy.utils.register_class(RotateUVRightOperator)
+    
+    kc = bpy.context.window_manager.keyconfigs.addon
+
+    km = kc.keymaps.new(name='Image', space_type='IMAGE_EDITOR')
+    km.keymap_items.new("uv.rotate_selection_left", 'R', 'PRESS', ctrl=True, shift=True)
+    
+    km = kc.keymaps.new(name='Image', space_type='IMAGE_EDITOR')
+    km.keymap_items.new("uv.rotate_selection_right", 'R', 'PRESS', shift=True)
+
+
+def unregister():
+    bpy.utils.unregister_class(NextTextBlockOperator)
+    bpy.utils.unregister_class(RotateUVRightOperator)
+    
+    kc = bpy.context.window_manager.keyconfigs.addon
+    kc.keymaps.remove(kc.keymaps['UV'])    
+
+
+if __name__ == "__main__":
+    register()
+
+
+
+
+
